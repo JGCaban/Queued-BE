d1b2bc05f56b90e9b2288d50753e4a07bfa7c95f
JGCaban <jose.caban317@gmail.com>
2019-01-07 12:14:41 -0500

    EventCreateRequest class

---
+++ src/SIS.API.DataContract/Event/EventCreateRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RedStarter.API.DataContract.Event
+{
+    public enum EventType { Sporting=1, Amusement, Product, Movie, Restaurant }
+    public class EventCreateRequest
+    {
+        public int EventId { get; set; }
+        public int JumperId { get; set; }
+        public int PlacerId { get; set; }
+        public EventType Type { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+        public string StartTime { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAssigned { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsExpired { get; set; }
+    }
+}
