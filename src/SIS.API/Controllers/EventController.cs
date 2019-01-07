91d56e1a5bd30ee10c7467ca6a2ecfb3dd147231
JGCaban <jose.caban317@gmail.com>
2019-01-07 15:18:32 -0500

    event mapping

--- src/SIS.API/Controllers/EventController.cs
+++ src/SIS.API/Controllers/EventController.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RedStarter.API.DataContract.Event;
+using RedStarter.Business.DataContract.Event;
 
 namespace RedStarter.API.Controllers
 {
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
     public class EventController : Controller
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+
+        public EventController(IMapper mapper)
         {
-            return View();
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostEvent(EventCreateRequest request)
+        {
+            var dto = _mapper.Map<EventCreateDTO>(request);
+            dto.DateCreated = DateTime.Now;
+
+            return Ok();
         }
     }
 }
